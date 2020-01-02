db183a34939f82b31bc51ce8399a4bf644cae53d
weijinyan <weijinyan@live.com>
2020-01-02 16:02:38 +0800

    rerunnable

--- files/yaml/k8s.sh
+++ files/yaml/k8s.sh
@@ -1,3 +1,7 @@
 #kubetcl create namespace test
+
+kubectl delete -f ./yaml/tomcat-rc.yaml --namespace=test
+kubectl delete -f ./yaml/tomcat-service.yaml --namespace=test
+
 kubectl apply -f ./yaml/tomcat-rc.yaml --namespace=test
 kubectl apply -f ./yaml/tomcat-service.yaml --namespace=test
